f1841fdc84679457dbac1050e5b1882378f93db1
stefanm95 <stefan.moise@andnet.ro>
2026-05-26 14:39:14 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <main className="bg-[#f6eadf] text-black overflow-hidden">
+      <section className="min-h-screen flex items-center justify-center">
+        <h1 className="text-7xl font-black">Selva De Go</h1>
+      </section>
+    </main>
+  );
+}
